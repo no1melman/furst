0e20e6a1f1b38a63fcf5c2e9f6424b2f2dc8aaa3
Callum Linington <callum.linington@live.co.uk>
2023-05-05 00:14:16 +0100

    build: add llvm build

--- build.ps1
+++ build.ps1
@@ -1,24 +1,87 @@
-$llvmVersion = "9fda8322243168cbfcb78c4cf80afa838473a573"
-$llvmSrc = "llvm.$llvmVersion"
+$ME="$((pwd).Path)"
+$depsSrcDir = "$ME/deps"
+$depsBuildDir = "$ME/deps_build"
 
-function Get-LLVM() {
-  Write-Host "Running: Get-LLVM"
-  $dest = "deps/$llvmSrc"
+$llvmVersion = "9fda8322243168cbfcb78c4cf80afa838473a573"
+$iwyuVersion="435ad9d35ceee7759ea8f8fd658579e979ee5146"
+$BDWGC_VERSION="release-8_2"
+
+$llvmDir = "llvm"
+$bdwgcDir = "bdwgc"
+$iwyuDir = "iwyu"
+$llvmSrc="$depsSrcDir/$llvmDir.$llvmVersion"
+$iwyuSrc="$depsSrcDir/$iwyuDir.$iwyuVersion"
+$bdwgcSrc = "$depsSrcDir/$bdwgcDir.$BDWGC_VERSION"
+
+$MUSL_VERSION="v1.2.3"
+
+# $winLibDir="C:/ProgramData/chocolatey/lib/winlibs/tools/mingw64/bin"
+# $CC="$winLibDir/clang.exe"
+# $CXX="$winLibDir/clang++.exe"
+
+function Check-Path() {
+  param(
+    [string]$Path
+  )
+
+  return Test-Path -Path $Path
+}
+
+function Make-Dir() {
+  param(
+    [string]$Path
+  )
+
+  if (!(Check-Path -Path $Path)) {
+    Write-Host "  Creating: $Path"
+    New-Item -ItemType Directory -Path $Path -Force
+  }
+}
+
+function Clone-Repo() {
+  param(
+    [string]$Dest,
+    [string]$Repo,
+    [string]$Version
+  )
+
   $itemCount = (Get-ChildItem -Path $dest -Recurse | Measure-Object).Count
-  Write-Host "There is an llvm... $itemCount"
+  Write-Host "Not Cloning: $Repo... $itemCount"
   if ($itemCount -gt 0) {
     return
   }
-
-  Write-Host "Cloning I guess..."
+  Write-Host "Cloning: $Repo"
   Write-Host ""
-  git clone --depth=1 https://github.com/llvm/llvm-project.git $dest
-  Push-Location $dest 
-  git fetch --depth=1 --filter=tree:0 origin "$llvmVersion"
+  git clone --depth=1 $Repo $Dest
+  Push-Location $Dest 
+  git fetch --depth=1 --filter=tree:0 origin "$Version"
   git reset --hard FETCH_HEAD
   Pop-Location
 }
 
+function Get-LLVM() {
+  Write-Host "Running: Get-LLVM"
+
+  $repo = "https://github.com/llvm/llvm-project.git"
+
+  Clone-Repo -Repo $repo -Dest $llvmSrc -Version $llvmVersion
+ }
+
+function Get-IWYU() {
+  Write-Host "Running: Get-IWYU"
+
+  $repo="https://github.com/include-what-you-use/include-what-you-use.git"
+
+  Clone-Repo -Repo $repo -Dest $iwyuSrc -Version $iwyuVersion
+}
+
+function Get-BDWGC() {
+  Write-Host "Running: Get-BDWGC"
+
+  $repo="https://github.com/ivmai/bdwgc.git"
+
+  Clone-Repo -Repo $repo -Dest $bdwgcSrc -Version $BDWGC_VERSION
+}
 
 function Build-Gen() {
   Write-Host "Running: Build-Gen"
@@ -36,5 +99,79 @@ function Build-Gen() {
   Pop-Location
 }
 
+function Build-BDWGC() {
+ 
+  $bdwgcBuildDir = "$depsBuildDir/${bdwgcDir}_build.$BDWGC_VERSION"
+  $bdwgcInstallDir = "$depsBuildDir/$bdwgcDir.$BDWGC_VERSION"
+
+  Make-Dir -Path $bdwgcBuildDir
+  Make-Dir -Path $bdwgcInstallDir
+  
+  Push-Location $bdwgcBuildDir
+  cmake -G Ninja `
+        -DCMAKE_INSTALL_PREFIX="$bdwgcInstallDir" `
+        -DBUILD_SHARED_LIBS=OFF `
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo `
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON `
+        -Dbuild_cord=ON `
+        -Denable_atomic_uncollectable=ON `
+        -Denable_cplusplus=OFF `
+        -Denable_disclaim=ON `
+        -Denable_docs=ON `
+        -Denable_dynamic_loading=ON `
+        -Denable_gc_assertions=ON `
+        -Denable_handle_fork=ON `
+        -Denable_java_finalization=OFF `
+        -Denable_munmap=ON `
+        -Denable_parallel_mark=ON `
+        -Denable_thread_local_alloc=ON `
+        -Denable_threads=ON `
+        -Denable_threads_discovery=ON `
+        -Denable_throw_bad_alloc_library=ON `
+        -Dinstall_headers=ON `
+        -DCMAKE_C_COMPILER="$CC" `
+        -DCMAKE_CXX_COMPILER="$CXX" `
+        "$bdwgcSrc"
+
+  cmake --build . --parallel
+  cmake -DCMAKE_INSTALL_PREFIX="$bdwgcInstallDir" -P cmake_install.cmake
+
+  Pop-Location
+}
+
+function Build-LLVM() {
+  Write-Host "Running: Build-LLVM"
+  $buildDir = "$depsSrcDir/$llvmDir.$llvmVersion"
+  $installDir = "$depsSrcDir/$llvmDir_build.$llvmVersion"
+  $TARGET_ARCHS="X86;AArch64;" # AMDGPU;ARM;RISCV;WebAssembly"
+  $env:TARGET_ARCHS=$TARGET_ARCHS
+
+  $llvmBuildDir = "$depsBuildDir/${llvmDir}_build.$llvmVersion"
+  $llvmInstallDir = "$depsBuildDir/$llvmDir.$llvmVersion"
+
+  Make-Dir -Path $llvmBuildDir
+  Make-Dir -Path $llvmInstallDir
+  
+  Push-Location $llvmBuildDir
+
+        # -DCMAKE_C_COMPILER="$CC" `
+        # -DCMAKE_CXX_COMPILER="$CXX" `
+  cmake -G Ninja `
+        -DCMAKE_INSTALL_PREFIX="$llvmInstallDir" `
+        -DLLVM_PARALLEL_COMPILE_JOBS=7 `
+        -DLLVM_PARALLEL_LINK_JOBS=1 `
+        -DTARGET_ARCHS="$TARGET_ARCHS" `
+        -C "$ME/cmake/caches/llvm.cmake" `
+        -S "$llvmSrc/llvm"
+  cmake --build . --parallel
+  cmake -DCMAKE_INSTALL_PREFIX="$llvmInstallDir" -P cmake_install.cmake
+
+  Pop-Location
+}
+
 Get-LLVM
-Build-Gen
+# Get-IWYU
+# Get-BDWGC
+# Build-BDWGC
+Build-LLVM
+# Build-Gen
---
+++ cmake/caches/llvm.cmake
@@ -0,0 +1,68 @@
+# This file sets up a CMakeCache for a LLVM toolchain build
+set(CMAKE_CXX_STANDARD 17)
+
+#Enable LLVM projects and runtimes
+set(LLVM_ENABLE_PROJECTS "clang;clang-tools-extra;lld;lldb;mlir" CACHE STRING "")
+set(LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind" CACHE STRING "")
+
+# Distributions should never be built using the BUILD_SHARED_LIBS CMake option.
+# That option exists for optimizing developer workflow only. Due to design and
+# implementation decisions, LLVM relies on global data which can end up being
+# duplicated across shared libraries resulting in bugs. As such this is not a
+# safe way to distribute LLVM or LLVM-based tools.
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
+
+#set(LLVM_BUILD_STATIC ON CACHE BOOL "")
+set(LLVM_BUILD_LLVM_DYLIB OFF CACHE BOOL "")
+set(LLVM_ENABLE_EH ON CACHE BOOL "")
+set(LLVM_ENABLE_RTTI ON CACHE BOOL "")
+
+set(LIBCLANG_BUILD_STATIC ON CACHE BOOL "")
+
+set(LLVM_ENABLE_LIBXML2 OFF CACHE BOOL "")
+set(LLVM_ENABLE_ASSERTIONS ON CACHE BOOL "")
+
+set(COMPILER_RT_USE_BUILTINS_LIBRARY ON CACHE BOOL "")
+
+# Create the builtin libs of compiler-rt
+set(COMPILER_RT_BUILD_BUILTINS ON CACHE BOOL "")
+set(COMPILER_RT_BUILD_STANDALONE_LIBATOMIC OFF CACHE BOOL "")
+set(COMPILER_RT_EXCLUDE_ATOMIC_BUILTIN OFF CACHE BOOL "")
+set(COMPILER_RT_CXX_LIBRARY "libcxx" CACHE STRING "")
+set(COMPILER_RT_USE_LIBCXX ON CACHE BOOL "Enable compiler-rt to use libc++ from the source tree")
+
+set(LIBUNWIND_ENABLE_STATIC ON CACHE BOOL "")
+set(LIBCXXABI_ENABLE_STATIC ON CACHE BOOL "")
+set(LIBCXX_DEFAULT_ABI_LIBRARY "libcxxabi" CACHE STRING "")
+set(LIBCXX_ENABLE_STATIC ON CACHE BOOL "")
+set(LIBCXX_USE_COMPILER_RT ON CACHE BOOL "")
+set(LIBCXX_CXX_ABI libcxxabi CACHE BOOL "")
+set(LIBCXXABI_USE_COMPILER_RT ON CACHE BOOL "")
+set(LIBCXXABI_USE_LLVM_UNWINDER ON CACHE BOOL "")
+set(LIBCXXABI_ENABLE_EXCEPTIONS ON CACHE BOOL "")
+set(LIBCXXABI_ENABLE_NEW_DELETE_DEFINITIONS ON CACHE BOOL
+  "Build libc++abi with definitions for operator new/delete. These are normally
+   defined in libc++abi, but it is also possible to define them in libc++, in
+   which case the definition in libc++abi should be turned off.")
+set(LIBCXX_ENABLE_NEW_DELETE_DEFINITIONS OFF CACHE BOOL "")
+set(LIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY ON CACHE BOOL " ")
+set(LIBCXX_HAS_GCC_LIB OFF CACHE BOOL "")
+set(LIBCXX_ENABLE_EXCEPTIONS ON CACHE BOOL "")
+set(LIBCXX_ENABLE_RTTI ON CACHE BOOL "")
+set(LIBCXX_ENABLE_THREADS ON CACHE BOOL "")
+
+set(LLVM_EXTERNAL_PROJECTS iwyu ON CACHE STRING "")
+
+# Only build the native target in stage1 since it is a throwaway build.
+set(LLVM_TARGETS_TO_BUILD ${TARGET_ARCHS} CACHE STRING "")
+
+# Optimize the stage1 compiler, but don't LTO it because that wastes time.
+set(CMAKE_BUILD_TYPE Release CACHE STRING "")
+
+# Setting up the stage2 LTO option needs to be done on the stage1 build so that
+# the proper LTO library dependencies can be connected.
+#set(LLVM_ENABLE_LTO OFF CACHE BOOL "")
+
+# Since LLVM_ENABLE_LTO is ON we need a LTO capable linker
+set(LLVM_ENABLE_LLD ON CACHE BOOL "")
+
